928d29b87867ae1114ea36a969c1358e74c92967
Benjamin Williams <benjaminwilliams@BenjamiiamssMBP.hsd1.or.comcast.net>
2017-04-21 12:41:12 -0700

    add correction, it passes first 2 specs though there are still lots of bugs and exceptions

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,8 +3,10 @@ require("rspec")
 require("pry")
 
 describe "String#anagram" do
-  it("Return true if two words are anagrams.") do
+  it("Returns false if two words are not anagrams.") do
+    expect("ruby".anagram("hard")).to(eq("False"))
+  end
+  it("Returns true if two words are anagrams.") do
     expect("ruby".anagram("bury")).to(eq("True"))
   end
-
 end
