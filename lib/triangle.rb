7fff09e03867460aa3467cdf8df30e0b0e1b6ef4
Ben Williams <benjarwilliams@gmail.com>
2017-04-24 13:52:27 -0700

    add complete project and triangle def in triangle.rb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,14 @@
 class Triangle
   def triCheck(side1, side2, side3)
-    if (side1 == side2 && side1 == side3)
-      "equilateral"
-    elsif (side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)
+    if (side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)
       "This isn't a triangle"
+    elsif (side1 == side2 && side1 == side3)
+      "Equilateral"
+    elsif (side1 == side2) || (side2 == side3) || (side1==side3)
+      "Isoceles"
     else
-      "omg it isn't equilateral"
+      "Scalene"
     end
   end
 end
+triangle = Triangle.new()
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -4,9 +4,15 @@ require("pry")
 describe "Triangle#triCheck" do
   triangle = Triangle.new
   it("recognizes if triangle is equilateral") do
-    expect(triangle.triCheck(3,3,3)).to(eql("equilateral"))
+    expect(triangle.triCheck(3,3,3)).to(eql("Equilateral"))
   end
   it("recognizes if a triangle is impossible") do
     expect(triangle.triCheck(3,3,7)).to(eql("This isn't a triangle"))
+  end
+  it("recognizes isoceles triangles") do
+    expect(triangle.triCheck(3,3,5)).to(eql("Isoceles"))
+  end
+  it("recognizes scalene triangles") do
+    expect(triangle.triCheck(2,3,4)).to(eql("Scalene"))
   end
 end
